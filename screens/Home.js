b98d4a432252663d3061d4a6da4c8f27837353f3
filippocinfriuni <filippo.cinfri.uni@gmail.com>
2023-03-23 11:54:12 +0100

    homepage

--- screens/Home.js
+++ screens/Home.js
@@ -1,8 +1,36 @@
-import { Text, View } from "react-native";
+import {
+  Text,
+  View,
+  FlatList,
+ } from "react-native";
 import { SafeAreaView, StyleSheet, Image } from 'react-native';
 import Ionicons       from "react-native-vector-icons/Ionicons";
 import React          from 'react';
 import { Card, Button }       from 'react-native-paper';
+
+const CHALLENGES = [
+  {
+    id: 1,
+    title: "Line-dry clothes",
+    content: "let the clothes dry naturally, save energy",
+    reward: 100,
+    buttonText: "complete",
+  },
+  {
+    id: 2,
+    title: "2",
+    content: "let the clothes dry naturally, save energy",
+    reward: 100,
+    buttonText: "complete",
+  },
+  {
+    id: 3,
+    title: "3",
+    content: "let the clothes dry naturally, save energy",
+    reward: 100,
+    buttonText: "complete",
+  },
+];
 
 
 const Score = () => {
@@ -16,9 +44,26 @@ const Score = () => {
     123 </Text> ;
 };
 
-
 const Icon = () => {
   return <Ionicons name={"flower-outline"} size={24} color={"tomato"} />;
+};
+
+
+const Carta = ({title, content, reward, buttonText}) => {
+  return <Card style={styles.card}>
+    <Text style={styles.title}> {title} </Text>
+    <Text style={styles.paragraph}> {content} </Text>
+    <Card.Actions>
+    <Text style={styles.paragraph}> {reward} </Text>
+      <Button>{content}</Button>
+    </Card.Actions>
+  </Card>
+}
+
+const renderItem = ({ item }) => {
+  return (
+    <Carta title={item.title} content={item.content} reward={item.reward} buttonText={item.buttonText}/>
+  );
 };
 
 
@@ -27,18 +72,13 @@ export default function HomeScreen() {
     <View style={styles.container}>
       <Text style={styles.score}> <Score /> <Icon /> </Text>
 
-      <Card>
-      <Text style={styles.title}>
-          Challenge 1
-        </Text>
-        <Text style={styles.paragraph}>
-          React Native Card View for Android and IOS using
-          "react-native-paper"
-        </Text>
-        <Card.Actions>
-          <Button>complete</Button>
-        </Card.Actions>
-      </Card>
+      <Carta title={"Challenge"} content={"testo react Native Card View for Android and IOS using react-native-paper"} reward={10} buttonText={"complete"}/>
+
+      <FlatList
+        data={CHALLENGES}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
 
     </View>
   );
@@ -50,6 +90,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     marginTop: 20,
     marginBottom: 20,
+  },
+  card: {
+    justifyContent: 'left',
+    margin: 20,
+    padding: 10,
   },
   container: {
     flex: 1,
