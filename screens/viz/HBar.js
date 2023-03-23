9dd8c9a37f00c62071a786bedb99de27fb7becb4
Paolo Marzolo <pollo.marzo@gmail.com>
2023-03-23 10:54:59 +0100

    dynamic rendering + removed title

---
+++ screens/viz/HBar.js
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function HBar({ data }) {
+  return <Text>{data}</Text>;
+}
---
+++ screens/viz/Water.js
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+export default function Water({ data }) {
+  return <Text>{data}</Text>;
+}
