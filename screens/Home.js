6d618e54ed5a04f1b4334ede84731956ebe9bb50
filippocinfriuni <filippo.cinfri.uni@gmail.com>
2023-03-23 18:06:44 +0100

    cards removable and score increase

--- screens/Home.js
+++ screens/Home.js
@@ -1,3 +1,4 @@
+
 import { Text, View, FlatList } from "react-native";
 import { SafeAreaView, StyleSheet, Image } from "react-native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -5,40 +6,7 @@ import Ionicons from "react-native-vector-icons/Ionicons";
 import React from "react";
 import { Card, Button, IconButton } from "react-native-paper";
 import { useState } from 'react';
-
-const App = () => {
-  const [elementVisible, setElementVisible] = useState(false);
-  return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <View
-        style={{
-          padding: 20,
-          flex: 1,
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-        }}
-      >
-        {elementVisible ? (
-          <View
-            style={{
-              backgroundColor: 'blue',
-              paddingHorizontal: 16,
-              paddingVertical: 8,
-              marginBottom: 20,
-            }}
-          >
-            <Text style={{ color: 'white', fontSize: 18 }}>Hello world!</Text>
-          </View>
-        ) : null}
-        <Button
-          title={elementVisible ? 'Hide Element' : 'Show Element'}
-          onPress={() => setElementVisible(!elementVisible)}
-        />
-      </View>
-    </SafeAreaView>
-  );
-};
+import { checkPluginState } from "react-native-reanimated/lib/reanimated2/core";
 
 const CHALLENGES = [
   {
@@ -52,7 +20,6 @@ const CHALLENGES = [
     title: "Go vegan for a day",
     content: "Try changing your food habits, eating veggies instead of meat will improve your carbon footprint",
     reward: 100,
-    
   },
   {
     id: 3,
@@ -69,6 +36,8 @@ const CHALLENGES = [
   },
 ];
 
+points = 130
+
 const Score = () => {
   return (
     <Text
@@ -79,42 +48,48 @@ const Score = () => {
         fontSize: 24,
       }}
     >
-      123{" "}
+      {points}
     </Text>
   );
 };
-
-const Stack = createNativeStackNavigator();
 
 const Icon = () => {
   return <Ionicons name={"flower-outline"} size={24} color={"tomato"} />;
 };
 
-const Carta = ({ title, content, reward, buttonText }) => {
-  return (
-    <Card style={styles.card}>
-      <Text style={styles.title}> {title} </Text>
-      <Text style={styles.paragraph}> {content} </Text>
-      <Card.Actions>
-        <Text style={styles.paragraph}> {reward} </Text>
-        <Button>{buttonText}</Button>
-      </Card.Actions>
-    </Card>
-  );
+const Carta = ({ title, content, reward, buttonText, todo, onPress }) => {
+      return (
+      <Card style={styles.card}>
+        <Text style={styles.title}>{title}</Text>
+        <Text style={styles.paragraph}>{content}</Text>
+        <Card.Actions>
+          <Text style={styles.paragraph}>{reward}</Text>
+          <Button onPress={onPress}>{buttonText}</Button>
+        </Card.Actions>
+      </Card>
+      );
 };
 
-const renderItem = ({ item }) => {
-  return (
-    <Carta
-      title={item.title}
-      content={item.content}
-      reward={item.reward}
-      buttonText={"complete"}
-    />
-  );
-};
+  
 
 export default function HomeScreen() {
+  const [items, setItems] = useState(CHALLENGES);
+  
+  const renderItem = ({ item }) => {
+    return (
+      <Carta
+        title={item.title}
+        content={item.content}
+        reward={item.reward}
+        buttonText={"complete"}
+        todo={item.todo}
+        onPress={() => { setItems(items.filter(i=>i.id!==item.id));
+                        points = points + item.reward}
+        }
+      />
+    );
+  };
+  
   return (
     <View style={styles.container}>
       <View style={styles.topContainer}>
@@ -125,24 +100,17 @@ export default function HomeScreen() {
           onPress={() => navigation.navigate("Footprint")}
         />
       </View>
-      
-      <App  />
-      <Text style={styles.score}>
-        aaaaaaaaaaaaaa
-      </Text>
 
 
       <Text style={styles.score}>
-        {" "} <Score /> <Icon />{" "}
+        <Score /><Icon />
       </Text>
 
       <FlatList
-        data={CHALLENGES}
+        data={items}
         renderItem={renderItem}
         keyExtractor={(item) => item.id}
       />
-    
-    
 
     </View>
     
@@ -198,3 +166,46 @@ const styles = StyleSheet.create({
     padding: 10,
   },
 });
+
+
+/*
+import React, { useState } from 'react';
+import { SafeAreaView, View, Text, Button } from 'react-native';
+
+const App = () => {
+  const [elementVisible, setElementVisible] = useState(true);
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View
+        style={{
+          padding: 20,
+          flex: 1,
+          display: 'flex',
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        {elementVisible ? (
+          <View
+            style={{
+              backgroundColor: 'blue',
+              paddingHorizontal: 16,
+              paddingVertical: 8,
+              marginBottom: 20,
+            }}
+          >
+            <Button
+          title={elementVisible ? 'Hide Element' : 'Show Element'}
+          onPress={() => setElementVisible(!elementVisible)}
+        />
+          </View>
+        ) : null}
+        
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default App;
+*/
