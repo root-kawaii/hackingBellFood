e6a52330b610c8201d07a1475fc45a3f8079f2f3
root-kawaii <teo.regge.99@gmail.com>
2023-03-23 17:44:41 +0100

    yoooo

--- screens/viz/Water.js
+++ screens/viz/Water.js
@@ -1,11 +1,10 @@
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 export default function Water({ data }) {
   return (
-    <Text>
-      {data}
+    <View>
       <Icon name="cow" size={150} color="#900" />
-    </Text>
+    </View>
   );
 }
