de235eeb696fc5d15bb260c80283701fb6abe9f7
Paolo Marzolo <pollo.marzo@gmail.com>
2023-03-23 11:13:25 +0100

    formatting and cow

--- screens/Home.js
+++ screens/Home.js
@@ -1,59 +1,58 @@
 import { Text, View } from "react-native";
-import { SafeAreaView, StyleSheet, Image } from 'react-native';
-import Ionicons       from "react-native-vector-icons/Ionicons";
-import React          from 'react';
-import { Card, Button }       from 'react-native-paper';
-
+import { SafeAreaView, StyleSheet, Image } from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import React from "react";
+import { Card, Button } from "react-native-paper";
 
 const Score = () => {
-  return <Text
-  style={{
-    textAlign: 'center',
-    color: 'black',
-    fontWeight: 'bold',
-    fontSize: 24,
-  }}>
-    123 </Text> ;
+  return (
+    <Text
+      style={{
+        textAlign: "center",
+        color: "black",
+        fontWeight: "bold",
+        fontSize: 24,
+      }}
+    >
+      123{" "}
+    </Text>
+  );
 };
-
 
 const Icon = () => {
   return <Ionicons name={"flower-outline"} size={24} color={"tomato"} />;
 };
 
-
 export default function HomeScreen() {
   return (
     <View style={styles.container}>
-      <Text> <Score /> <Icon /> </Text>
-
+      <Text>
+        <Score /> <Icon />
+      </Text>
       <Card>
         <Text style={styles.paragraph}>
-          React Native Card View for Android and IOS using
-          "react-native-paper"
+          React Native Card View for Android and IOS using "react-native-paper"
         </Text>
         <Card.Actions>
           <Button>Cancel</Button>
           <Button>Ok</Button>
         </Card.Actions>
       </Card>
-
     </View>
   );
 }
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: "center",
     padding: 20,
-    backgroundColor: '#ecf0f1',
+    backgroundColor: "#ecf0f1",
   },
   paragraph: {
     fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    padding: 20
+    fontWeight: "bold",
+    textAlign: "center",
+    padding: 20,
   },
 });
--- screens/Rank.js
+++ screens/Rank.js
@@ -1,11 +1,9 @@
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
-import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
 export default function RankScreen() {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Details Screen!</Text>
-      <Icon name="cow" size={300} color="#900" />;
     </View>
   );
 }
