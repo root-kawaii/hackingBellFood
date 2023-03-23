b80ab75be8d9ace69c25b6c5ef6bf35e932e4054
filippocinfriuni <filippo.cinfri.uni@gmail.com>
2023-03-23 10:51:58 +0100

    homepage

--- screens/Home.js
+++ screens/Home.js
@@ -1,9 +1,59 @@
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import { Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Image } from 'react-native';
+import Ionicons       from "react-native-vector-icons/Ionicons";
+import React          from 'react';
+import { Card, Button }       from 'react-native-paper';
+
+
+const Score = () => {
+  return <Text
+  style={{
+    textAlign: 'center',
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 24,
+  }}>
+    123 </Text> ;
+};
+
+
+const Icon = () => {
+  return <Ionicons name={"flower-outline"} size={24} color={"tomato"} />;
+};
+
 
 export default function HomeScreen() {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>StartHack 2023!</Text>
+    <View style={styles.container}>
+      <Text> <Score /> <Icon /> </Text>
+
+      <Card>
+        <Text style={styles.paragraph}>
+          React Native Card View for Android and IOS using
+          "react-native-paper"
+        </Text>
+        <Card.Actions>
+          <Button>Cancel</Button>
+          <Button>Ok</Button>
+        </Card.Actions>
+      </Card>
+
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#ecf0f1',
+  },
+  paragraph: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    padding: 20
+  },
+});
